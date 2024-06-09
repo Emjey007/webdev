0e0eed21539269b285ff3f838c37b4bc2592be58
SandorTheMoon <144748867+SandorTheMoon@users.noreply.github.com>
2024-06-10 00:29:58 +0800

    fixed

--- myapp/main/admin.py
+++ myapp/main/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import AllEquipment, EquipmentOnMission
+from .models import UserClassification, AllEquipment, EquipmentOnMission
 
 # Register your models here.
+admin.site.register(UserClassification)
 admin.site.register(AllEquipment)
 admin.site.register(EquipmentOnMission)
+

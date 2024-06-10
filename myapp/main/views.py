e679eb3d0d1a49362829c38d46845d608ee10b3c
Mark Jhay Corpuz <markjhaycorpuzz@gmail.com>
2024-06-10 19:26:36 +0800

    The 2 database models are fixed
    New update on status of equipment on all equipment page and it can be duplicated in other models.

--- myapp/main/views.py
+++ myapp/main/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .forms import RegistrationForm, EquipmentForm
-from .models import UserClassification, AllEquipment, EquipmentOnMission
+from .models import UserClassification, AllEquipment, EquipmentOnMission, DamagedEquipment
 
 # Create your views here.
 
@@ -46,11 +46,41 @@ def add_equipment(request):
     return render(request, 'add_equipment.html', { 'equipment_form': equipment_form })
 
 def all_equipment(request):
-    equipments = AllEquipment.objects.filter
+    all_equipment = AllEquipment.objects.all()
+    return render(request, 'all_equipment.html', {'all_equipment': all_equipment})
 
-def available_equipment(request):
-    available_equipments = AllEquipment.objects.filter(is_available=True)
-    return render(request, 'available_equipment.html', {'available_equipments': available_equipments})
+def equipment_on_mission(request):
+    equipment_on_mission = EquipmentOnMission.objects.all()
+    return render(request, 'equipment_on_mission.html', {'equipment_on_mission': equipment_on_mission})
+
+def damaged_equipment(request):
+    damaged_equipment = DamagedEquipment.objects.all()
+    return render(request, 'damaged_equipment.html', {'damaged_equipment': damaged_equipment})
+
+# Try
+def duplicate_equipment(request, equipment_id):
+    equipment = get_object_or_404(AllEquipment, id=equipment_id)
+    EquipmentOnMission.objects.create(
+        all_equipment=equipment,
+        equipment_id_on_mission=equipment.equipment_id,
+        equipment_model=equipment.equipment_model,
+        equipment_category=equipment.equipment_category,
+        equipment_name=equipment.equipment_name,
+        equipment_description=equipment.equipment_description,
+    )
+    return redirect('all_equipment')
+
+def duplicate_equipment1(request, equipment_id):
+    equipment = get_object_or_404(AllEquipment, id=equipment_id)
+    DamagedEquipment.objects.create(
+        all_equipment=equipment,
+        equipment_id_on_damaged=equipment.equipment_id,
+        equipment_model=equipment.equipment_model,
+        equipment_category=equipment.equipment_category,
+        equipment_name=equipment.equipment_name,
+        equipment_description=equipment.equipment_description,
+    )
+    return redirect('all_equipment')
 
 # def move_equipment(request):
 #     if request.method == 'POST':
@@ -70,4 +100,3 @@ def available_equipment(request):
 
 #     equipment_list = AllEquipment.objects.all()
 #     return render(request, 'move_equipment.html', {'equipment_list': equipment_list})
-
